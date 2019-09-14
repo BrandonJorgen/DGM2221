018463d8423c24994568b99529d3d7bb5b511ca4
Brandon <brandonjorgen@gmail.com>
2019-09-14 09:13:01 -0600

    Added Whitebox VFX

---
+++ Oddworld Environment/Assets/Scripts/AnimEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimEvent : MonoBehaviour
+{
+    public UnityEvent animEvent;
+
+    public void ExecEvent()
+    {
+        animEvent.Invoke();
+    }
+}
